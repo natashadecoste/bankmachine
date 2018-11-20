ac31ccb4b8b58228aa5d1f0b3e2165bddcf4d7bb
Natasha DeCoste <natasha.decoste@gmail.com>
2018-11-19 20:28:18 -0500

    fixed error removing materialize

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,1 @@
 import "./src/globalstyles/reset.css"
-import "./src/globalstyles/materialize/css/materialize.min.css"
--- src/component/summary.js
+++ src/component/summary.js
@@ -1,28 +1,28 @@
 import React from "react";
-import PropTypes from "prop-types"
-import "./component-styles.scss"
+import PropTypes from "prop-types";
+import "./component-styles.scss";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 export class Summary extends React.Component {
-    render(){
-        return (
-            <div className="section-container">
-                <h1>Account Summary</h1>
-                <ul className="summary-list">
-                    {this.props.accounts.map((account, index) => 
-                        <li 
-                        key={`item--${index}`}
-                        className="account-summary-li" >
-                            {account.name}
-                        </li>
-                    )}
-                </ul>
-            </div>
-        );
-    }
-
+  render() {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <div className="section-container">
+          <h1>Account Summary</h1>
+          <ul className="summary-list">
+            {this.props.accounts.map((account, index) => (
+              <li key={`item--${index}`} className="account-summary-li">
+                {account.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 Summary.propTypes = {
-    accounts: PropTypes.array,
-
-}
+  accounts: PropTypes.array
+};
