3566032d614c4e98dd31346c599f371d1f0b95e5
Natasha DeCoste <natasha.decoste@gmail.com>
2018-11-19 20:20:57 -0500

    add material from npm

--- src/pages/session.js
+++ src/pages/session.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { Summary, Deposit, Transfer } from "./../component/";
 import { Page } from "../layouts/page";
+//import Material from "material";
 
 export default class Session extends React.Component {
   account1 = { balance: 400, name: "Chequeing" };
@@ -11,6 +12,23 @@ export default class Session extends React.Component {
   constructor(props) {
     super(props);
     this.state = { currentPage: "transfer" };
+
+    
+//   document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = Material.Sidenav.init(elems, options);
+//   });
+
+  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+  // var collapsibleElem = document.querySelector('.collapsible');
+  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+  // Or with jQuery
+
+//   $(document).ready(function(){
+//     $('.sidenav').sidenav();
+//   });
+        
   }
 
   render() {
@@ -18,7 +36,18 @@ export default class Session extends React.Component {
       <Page>
         This will hold all the values for the session
         <br />
-        {this.state.currentPage === ("summary" || null) && (
+        <nav> nothing </nav>
+
+<ul id="slide-out" class="sidenav-fixed">
+  <li><i class="material-icons">Deposit</i>First Link With Icon</li>
+  <li>Second Link</li>
+  <li><div class="divider"></div></li>
+  <li><a class="subheader">Subheader</a></li>
+  <li>Third Link With Waves</li>
+</ul>
+<a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    
+        {/* {this.state.currentPage === ("summary" || null) && (
           <Summary accounts={this.accounts} />
         )}
         {this.state.currentPage === "deposit" && (
@@ -26,7 +55,7 @@ export default class Session extends React.Component {
         )}
         {this.state.currentPage === "transfer" && (
           <Transfer accounts={this.accounts} />
-        )}
+        )} */}
       </Page>
     );
   }
