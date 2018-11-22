de46039238353ba880ff29ae54bddbc0d4aeb487
Troy Kuang <troykuang0608@gmail.com>
2018-11-22 16:10:07 -0500

    transfer page

--- src/component/transfer.js
+++ src/component/transfer.js
@@ -1,8 +1,30 @@
 import React from "react";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 import "./component-styles.scss"
+import {Dropdown} from './dropdown';
 
 export class Transfer extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          selectedAccount: props.accounts[0],
+          selectedCurrency: "CAD"
+        };
+      }
+    
+      selectItem = (item) => {
+          this.setState({
+              selectedAccount: item
+          });
+    
+      }
+    
+      selectCurrency = (currency) => {
+        this.setState({
+            selectedCurrency: currency.name
+        });
+      }
 
     render(){
         return (
@@ -10,7 +32,8 @@ export class Transfer extends React.Component {
                 <h1>Transfer from Account to Account</h1>
                 
                 <div>Transfer from:
-                    <ul className="account-list">
+                    <Dropdown label="Select Account:" select={this.selectItem} selected={this.state.selectedAccount} list={this.props.accounts}></Dropdown>
+                    {/* <ul className="account-list">
                         {this.props.accounts.map((account, index) => 
                             <li 
                             key={`item--${index}`}
@@ -18,15 +41,15 @@ export class Transfer extends React.Component {
                                 {`${account.name}--${account.balance}`}
                             </li>
                         )}
-                    </ul>
-                
-                How much money to Transfer?
-                    <input type="text" placeholder="e.g. 45.22 or 45"></input>
+                    </ul> */}
                 </div>
 
                                 
                 <div>Transfer to:
-                    <ul className="account-list">
+                <Dropdown label="Select Account:" select={this.selectItem} selected={this.state.selectedAccount} list={this.props.accounts}></Dropdown>
+                    {/* <Dropdown label="Select Account:" select={this.selectItem} selected={this.state.selectedAccount} list={this.props.accounts}></Dropdown>
+  */}
+                    {/* <ul className="account-list">
                         {this.props.accounts.map((account, index) => 
                             <li 
                             key={`item--${index}`}
@@ -34,8 +57,12 @@ export class Transfer extends React.Component {
                                 {`${account.name}--${account.balance}`}
                             </li>
                         )}
-                    </ul>
+                    </ul> */}
+                    
                 </div>
+                How much money to Transfer?
+                    <input type="text" placeholder="e.g. 45.22 or 45"></input><Dropdown select={this.selectCurrency} list={[{"name": "CAD"}, {"name": "USD"}, {"name": "EUR"}]}></Dropdown>
+
             </div>
         );
     }
