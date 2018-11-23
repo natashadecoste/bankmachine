a44d37e0a391054deffdced1b5f8010c2f7f8700
Natasha DeCoste <natasha.decoste@gmail.com>
2018-11-22 23:29:25 -0500

    finish login page

--- src/component/login.js
+++ src/component/login.js
@@ -1,17 +1,30 @@
 import React from "react";
-import "./component-styles.scss"
+import "./component-styles.scss";
+import { TextField } from "@material-ui/core/";
 
 export class Login extends React.Component {
-    validation = () => {
-
-    }
-    render(){
-        return (
-            <div className="login-form">
-                <input type="text" placeholder="Account Number"></input>
-                <input type="password" placeholder="Password"></input>
-            </div>
-        );
-    }
-
+  validation = () => {};
+  render() {
+    return (
+      <div className="login-form">
+        <TextField
+          required
+          fullWidth
+          id="standard-required"
+          label="Account Number"
+          defaultValue=""
+          margin="normal"
+        />
+        <TextField
+          fullWidth
+          required
+          id="standard-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          margin="normal"
+        />
+      </div>
+    );
+  }
 }
