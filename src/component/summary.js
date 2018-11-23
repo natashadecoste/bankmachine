e9d5cb11b58741d802a88ed8a90e5904c8f63415
Natasha DeCoste <natasha.decoste@gmail.com>
2018-11-23 01:57:37 -0500

    format account summary

--- src/component/summary.js
+++ src/component/summary.js
@@ -2,6 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./component-styles.scss";
 
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow
+} from "@material-ui/core/";
 
 export class Summary extends React.Component {
   render() {
@@ -10,14 +17,32 @@ export class Summary extends React.Component {
         <h1>Account Summary</h1>
 
         <div>
-          Accounts:
-          <ul className="account-list">
-            {this.props.accounts.map((account, index) => (
-              <li key={`item--${index}`} className="account-summary-li">
-                {`${account.name}--${account.balance}`}
-              </li>
-            ))}
-          </ul>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Account Name</TableCell>
+                <TableCell numeric>Balance</TableCell>
+                {/* <TableCell numeric>Fat (g)</TableCell>
+                <TableCell numeric>Carbs (g)</TableCell>
+                <TableCell numeric>Protein (g)</TableCell> */}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.props.accounts.map((account, index) => {
+                return (
+                  <TableRow key={`row--${index}`}>
+                    <TableCell component="th" scope="row">
+                      {account.name}
+                    </TableCell>
+                    <TableCell numeric>{account.balance}</TableCell>
+                    {/* <TableCell numeric>{row.fat}</TableCell>
+                <TableCell numeric>{row.carbs}</TableCell>
+                <TableCell numeric>{row.protein}</TableCell> */}
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
         </div>
       </div>
     );
