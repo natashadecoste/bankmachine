938773f1083ecc3b6cc92a9d47b062dd95806c9a
OwenMitchell <owenrossmitchell@gmail.com>
2018-11-27 19:32:44 -0500

    exchange page how displays a table of all saved accounts' balances in selected currency

--- src/component/currencyexchange.js
+++ src/component/currencyexchange.js
@@ -3,6 +3,14 @@ import PropTypes from "prop-types";
 import "./component-styles.scss";
 import { Dropdown } from "./dropdown";
 
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow
+} from "@material-ui/core/";
+
 export class CurrencyExchange extends React.Component {
   constructor(props) {
     super(props);
@@ -10,9 +18,22 @@ export class CurrencyExchange extends React.Component {
     this.state = {
       selectedAccount: props.accounts[0],
       selectedCurrency: "CAD",
-      exchangeRate: 1
+      exchangeRate: 1,
+      accounts: props.accounts
     };
   }
+
+  createExchangeTable = (exchangeRate) => {
+    var table = [];
+    this.state.accounts.forEach(function(account) {
+      var row = [];
+      row.push(<TableCell>{account.name}</TableCell>);
+      row.push(<TableCell>{account.balance * exchangeRate}</TableCell>);
+      table.push(<TableRow>{row}</TableRow>);
+    });
+
+    return table;
+  };
 
   selectItem = item => {
     this.setState({
@@ -27,30 +48,32 @@ export class CurrencyExchange extends React.Component {
     });
   };
 
+
+
   render() {
     return (
       <div className="section-container">
         <h1>Compare Value of Your Accounts in Different Currencies</h1>
-        <div className="input-group">
-          <Dropdown
-            label="Select Account:"
-            select={this.selectItem}
-            selected={this.state.selectedAccount}
-            list={this.props.accounts}
-          />
-        </div>
 
         <div className="input-group">
           What currency would you like to use?
           <Dropdown
             select={this.selectCurrency}
-            list={[{ name: "CAD" }, { name: "USD" }, { name: "EUR" }]}
+            list={[{ name: "CAD", rate: 1 }, { name: "USD", rate: 0.75 }, { name: "EUR", rate: .67 }]}
           />
         </div>
         <h1>
-          The Value of your {this.state.selectedAccount.name} account in{" "}
-          {this.state.selectedCurrency} is{" "}
+          Accounts:
         </h1>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Account</TableCell>
+              <TableCell>Balance in {this.state.selectedCurrency}</TableCell>
+            </TableRow>
+          </TableHead>
+        {this.createExchangeTable(this.state.exchangeRate)}
+        </Table>
       </div>
     );
   }
